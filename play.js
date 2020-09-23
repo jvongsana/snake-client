1be435764288ff5cbc5e664effe110160c995c16
Jason Vongsana <jvongsana@hotmail.com>
2020-09-23 21:12:53 +0000

    Added user input and cancelation of node

--- play.js
+++ play.js
@@ -1,3 +1,17 @@
 const connect = require('./client');
 console.log('Connecting ...');
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  const handleUserInput = stdin.on('data', (data) => {
+    if (data == '\\q\n') {
+      process.exit();
+    }
+    stdin.resume();
+    setupInput();
+  });
+  return stdin;
+};
